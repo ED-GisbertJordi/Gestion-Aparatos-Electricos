69fa052ccb60e0c1e8785945c320390f65a1b59f
JordiGis <jorgisfer@alu.edu.gva.es>
2024-03-26 00:02:51 +0100

    Repositorio

---
+++ src/electronicos/GestionAparatosElectronicos.java
@@ -0,0 +1,16 @@
+package electronicos;
+
+/**
+ * @author Jordi Gisbert y Jean Marc 
+ */
+public class GestionAparatosElectronicos {
+    
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
+    
+    
+}
