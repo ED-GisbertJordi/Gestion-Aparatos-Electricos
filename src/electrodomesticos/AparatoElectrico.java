eb130415e335ba805555dedc968a117530488164
JordiGis <jorgisfer@alu.edu.gva.es>
2024-03-26 01:58:03 +0100

    Add class AparatoElectronico, Movil

---
+++ src/electrodomesticos/AparatoElectrico.java
@@ -0,0 +1,24 @@
+package electrodomesticos;
+
+/**
+ * @author Jordi Gisbert y Jean Marc 
+ */
+public class AparatoElectrico {
+    private int numeroSerie;
+    private boolean encendido = false;
+    private boolean tieneCorreinteElecctrica = false;
+    private boolean conectadoInternet = false;
+    private String marca;
+    private String modelo;
+
+    public AparatoElectrico(int numeroSerie, String marca, String modelo) {
+        this.numeroSerie = numeroSerie;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/electrodomesticos/Movil.java
@@ -0,0 +1,17 @@
+package electrodomesticos;
+
+/**
+ * @author Jordi Gisbert y Jean Marc 
+ */
+public class Movil extends AparatoElectrico{
+    private MemoriaInterna memoriaInterna;
+
+    public Movil(MemoriaInterna memoriaInterna, int numeroSerie, String marca, String modelo) {
+        super(numeroSerie, marca, modelo);
+        this.memoriaInterna = memoriaInterna;
+    }
+
+    
+}
+
+enum MemoriaInterna {Mb64, Mb128, Mb256, Mb512, Gb1, Gb2}
