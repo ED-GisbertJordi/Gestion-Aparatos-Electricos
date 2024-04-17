4e78abd97387b1f54a5138281b156ec256bed5eb
JordiGis <jorgisfer@alu.edu.gva.es>
2024-04-17 18:27:12 +0200

    UP TestRouter formato

--- src/main/TestRouter.java
+++ src/main/TestRouter.java
@@ -21,15 +21,15 @@ public class TestRouter {
         Regleta regleta = new Regleta();
         Router internet = new Router("1233C");
 
-        System.out.println("----------Aparatos conectables creados ----------");
+        System.out.println("\n----------Aparatos conectables creados ----------\n");
         System.out.println(l1 + "\n" + l2 + "\n" + t1 + "\n" + t2 + "\n" + c1 + "\n" + c2 + "\n" + m1 + "\n" + m2);
 
-        System.out.println("----------Encender televisión, móvil y ordenador----------");
+        System.out.println("\n----------Encender televisión, móvil y ordenador----------\n");
         t1.activar();
         m1.activar();
         c1.activar();
 
-        System.out.println("-----------Conectar televisión a una regleta y encender la TV----------");
+        System.out.println("\n-----------Conectar televisión a una regleta y encender la TV----------\n");
 
         if (regleta.enchufar(t2)) {
             System.out.println("[Televisión] Aparato enchufado");
@@ -38,37 +38,96 @@ public class TestRouter {
         }
         t2.activar();
 
-        System.out.println("-----------Crear router y emparejar los tres aparatos. Televisión no podrá por no tener habilitado Wifi----------");
+        System.out.println("\n-----------Crear router y emparejar los tres aparatos. Televisión no podrá por no tener habilitado Wifi----------\n");
         t1.deshabilitarWifi();
 
-        if (internet.emparejar(t1)) {
-            System.out.println("El router no está disponible (sin corriente o no encendido)");
-        } else {
-            System.out.println("El router está disponible");
-        }
+        internet.emparejar(t1);
 
-        if (internet.emparejar(m2)) {
-            System.out.println("El router no está disponible (sin corriente o no encendido)");
-        } else {
-            System.out.println("El router está disponible");
-        }
+        internet.emparejar(m2);
 
-        if (internet.emparejar(c2)) {
-            System.out.println("El router no está disponible (sin corriente o no encendido)");
-        } else {
-            System.out.println("El router está disponible");
-        }
+        internet.emparejar(c2);
 
-        System.out.println("----------Habilitar Wifi para televisión y emparejar con router----------");
+        System.out.println("\n----------Habilitar Wifi para televisión y emparejar con router----------\n");
         t1.habilitarWifi();
 
-       internet.emparejar(t1);
-        
-        System.out.println("----------Desemparejar móvil y mostrar los aparatos emparejados----------");
+        internet.emparejar(t1);
+
+        System.out.println("\n----------Desemparejar móvil y mostrar los aparatos emparejados----------\n");
         internet.desemparejar(m2);
         internet.listarDispositivosEmparejados();
-         
+
+        System.out.println("\n----------Desenchufar televisión y listar aparatos emparejados----------\n");
+        t1.activar();
+        internet.listarDispositivosEmparejados();
+        internet.listarDispositivosEmparejados();
+
+        System.out.println("\n----------Aparatos conectables creados----------\n");
+        System.out.println(l1 + "\n" + l2 + "\n" + t1 + "\n" + t2 + "\n" + c1 + "\n" + c2 + "\n" + m1 + "\n" + m2);
+
+        System.out.println("\n----------Encender televisión, móvil y ordenador----------\n");
+        t1.activar();
+        m1.activar();
+        c1.activar();
+
+        System.out.println("\n-----------Conectar televisión a una regleta y encender la TV----------\n");
+        if (regleta.enchufar(t1)) {
+            System.out.println("[Televisión] Aparato enchufado");
+        } else {
+            System.out.println("[Televisión] No se ha podido enchufar");
+        }
+        t1.activar();
+        
+        System.out.println("\n-----------Crear router, conectarlo a la regleta y encenderlo----------------\n");
+        if (regleta.enchufar(internet)) {
+            System.out.println("[Router] Aparato enchufado");
+        } else {
+            System.out.println("[Router] No se ha podido enchufar");
+        }
+        internet.activar();
+   
+        System.out.println("\n-----------Emparejar los tres aparatos. Televisión no podrá por no tener habilitado Wifi----------\n");
+        if (internet.emparejar(t2)) {
+            System.out.println("[Televisión] No se ha podido establecer la conexión (sin corriente, apagado o la interfaz wifi no está habilitada)");
+        } else {
+            System.out.println("[Televisión] Conexion establecida");
+        }
+
+        if (internet.emparejar(c1)) {
+            System.out.println("[Computador] Aparato emparejado con éxito");
+        } else {
+            System.out.println("[Computador] Aparato no emparejado");
+        }
+        if (internet.emparejar(m1)) {
+            System.out.println("[Móvil] Aparato emparejado con éxito");
+        } else {
+            System.out.println("[Móvil] Aparato no emparejado");
+        }
+
+        System.out.println("\n----------Habilitar Wifi para televisión y emparejar con router----------\n");
+        t2.habilitarWifi();
+        if (!internet.emparejar(t2)) {
+            System.out.println("[Televisión] Aparato emparejado con éxito");
+        } else {
+            System.out.println("[Televisión] Aparato no emparejado");
+        }
+
+        System.out.println("\n----------Desemparejar móvil y mostrar los aparatos emparejados----------\n");
+        if (internet.desemparejar(m1)) {
+            System.out.println("[Móvil] Aparato desemparejado con éxito");
+        } else {
+            System.out.println("[Móvil] No se ha podido desemparejar");
+        }
+        internet.listarDispositivosEmparejados();
+
+        System.out.println("\n----------Desenchufar televisión y listar aparatos emparejados----------\n");
+        if (regleta.desenchufar(t2)) {
+            System.out.println("[Televisión] Aparato desenchufado");
+        } else {
+            System.out.println("[Televisión] Aparato no desenchufado");
+        }
+        internet.actualizarDispositivosEmparejados();
+        internet.listarDispositivosEmparejados();
+
     }
-    
     
 }
